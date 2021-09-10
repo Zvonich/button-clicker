85275a07cfbdfc38cd6415225e9e5c0a29c8cf42
Zvonich <90396162+Zvonich@users.noreply.github.com>
2021-09-10 20:49:11 +0200

    Update Point.js

--- Point.js
+++ Point.js
@@ -2,16 +2,13 @@ function incrementButton1() {
   var element = document.getElementById("IncrementText");
   var value = element.innerHTML;
   ++value;
-  console.log(value);
   document.getElementById("IncrementText").innerHTML = value
 }
 
 
 function incrementButton10() {
-  let x = 9
   var element = document.getElementById("IncrementText");
   var value = element.innerHTML;
-  x++value;
-  console.log(value);
+  ++value;
   document.getElementById("IncrementText").innerHTML = value
 }
