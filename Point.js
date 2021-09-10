0d09c8a91b73defc09ae3cbbf6e56f03ff30a0d6
Zvonich <90396162+Zvonich@users.noreply.github.com>
2021-09-10 20:11:53 +0200

    Update Point.js

--- Point.js
+++ Point.js
@@ -7,3 +7,10 @@ function incrementButton1() {
 }
 
 
+function incrementButton10() {
+  var element = document.getElementById("IncrementText");
+  var value = element.innerHTML;
+  ++value;
+  console.log(value);
+  document.getElementById("IncrementText").innerHTML = value
+}
