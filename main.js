c08748ad90cc2e0b966eab301af4063abdd8320d
Zvonich <90396162+Zvonich@users.noreply.github.com>
2021-09-11 12:01:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var cookies = 0;
 
-function cookieClick(number){
-    cookies = cookies + number;
+function cookieClick(10){
+    cookies = cookies + 10;
     document.getElementById("cookies").innerHTML = cookies;
 };
 
