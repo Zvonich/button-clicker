43b916c1e586ebd1ea73b6b99021c3da47e80855
Zvonich <90396162+Zvonich@users.noreply.github.com>
2021-09-11 15:29:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-var cookies = 0;
+var points = 0;
 
-function cookieClick(number){
-    cookies = cookies + number;
-    document.getElementById("cookies").innerHTML = cookies;
-};
+function pointClick(number){
+    points = points + number;
+    document.getElementById("points").innerHTML = points;
 
+};   
+
+document.getElementById("10p").style.display = "none"
+var mainGameLoop = window.setInterval(function() {
+    if(points >= 10) {                                                           //change 10 when launching game
+        document.getElementById("10p").style.display = "inline-block"
+    }
+
+  }, 1000)
+ 
+document.getElementById("100p").style.display = "none"
+var mainGameLoop = window.setInterval(function() {
+    if(points >= 100) {                                                           //change 100 when launching game
+        document.getElementById("100p").style.display = "inline-block"
+    }
+
+  }, 1000)
+
+document.getElementById("1000p").style.display = "none"
+var mainGameLoop = window.setInterval(function() {
+      if(points >= 1000) {                                                           //change 1000 when launching game
+          document.getElementById("1000p").style.display = "inline-block"
+      }
+  
+    }, 1000)
+   
